e32a14fb80fb3d12b1b22eb69d09a9d2380e3f92
Junda Chen <32371474+GindaChen@users.noreply.github.com>
2025-03-10 23:36:20 -0700

    Update setup-runpod.sh

--- setup-runpod.sh
+++ setup-runpod.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+set -x
+
+echo -e "\033[32m   _____ _           _        _____ _                 \033[0m
+\033[32m  / ____(_)         | |      / ____| |                \033[0m
+\033[32m | |  __ _ _ __   __| | __ _| |    | |__   ___ _ __  \033[0m
+\033[33m | | |_ | | '_ \ / _\` |/ _\` | |    | '_ \ / _ \ '_ \ \033[0m
+\033[31m | |__| | | | | | (_| | (_| | |____| | | |  __/ | | |\033[0m
+\033[31m  \_____|_|_| |_|\__,_|\__,_|\_____|_| |_|\___|_| |_|\033[0m"
+
+
+
 apt-get update 
 apt-get install -y curl wget git tmux tree less
 
-git clone https://github.com/GindaChen/dotfiles.git ~/dotfiles
+git clone https://github.com/GindaChen/dotfiles.git ~/dotfiles || true
 
-cd ~/dotfiles/runpod && bash ./runpod-setup.sh && cd -
+cd ~/dotfiles/runpod && bash ./setup.sh && cd -
+
+echo "Done"
+
+set +x
