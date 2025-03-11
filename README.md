c8124c4e297de732b86227029fb943fc5829bb2a
Junda Chen <32371474+GindaChen@users.noreply.github.com>
2025-03-11 00:15:23 -0700

    add some verification

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 ## Runpod
 
+> **Note**: Make sure to replace `REPO_NAME=GindaChen/dotfiles` with your own repository name in the command below if you've forked this repository. The setup script will configure git with the credentials specified in `runpod/setup_git.sh`.
+
+
+After starting the runpod, run the following command:
+```bash
+REPO_NAME="YourUsername/YourRepo" 
+curl -sSL https://raw.githubusercontent.com/${REPO_NAME}/refs/heads/master/setup-runpod.sh | bash -s ${REPO_NAME} 
 ```
-curl -sSL https://raw.githubusercontent.com/GindaChen/dotfiles/refs/heads/master/setup-runpod.sh | bash
 
-
+For example, if you are @GindaChen with dotfiles repository, you should run:
+```bash
+curl -sSL https://raw.githubusercontent.com/GindaChen/dotfiles/refs/heads/master/setup-runpod.sh | bash -s "GindaChen/dotfiles" 
 ```
--- setup-runpod.sh
+++ setup-runpod.sh
@@ -9,12 +9,19 @@ echo -e "\033[32m   _____ _           _        _____ _                 \033[0m
 \033[31m | |__| | | | | | (_| | (_| | |____| | | |  __/ | | |\033[0m
 \033[31m  \_____|_|_| |_|\__,_|\__,_|\_____|_| |_|\___|_| |_|\033[0m"
 
-
+if [ -z "$1" ]; then
+    echo "Error: Repository name is required."
+    echo "Usage: $0 YourUsername/YourRepo"
+    echo "For example, if you are @GindaChen with dotfiles repository, you should run:"
+    echo "curl -sSL https://raw.githubusercontent.com/GindaChen/dotfiles/refs/heads/master/setup-runpod.sh | bash -s GindaChen/dotfiles"
+    exit 1
+fi
+REPO_NAME=$1
 
 apt-get update 
 apt-get install -y curl wget git tmux tree less
 
-git clone https://github.com/GindaChen/dotfiles.git ~/dotfiles || true
+git clone https://github.com/${REPO_NAME}.git ~/dotfiles || true
 
 bash ~/dotfiles/runpod/setup.sh || true && cd -
 
